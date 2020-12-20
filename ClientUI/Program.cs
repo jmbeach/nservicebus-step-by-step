d31551197537d16503ab1e161bbca9922c6e5acf
jmbeach <jaredbeachdesign@gmail.com>
2020-12-19 20:59:34 -0600

    adds placeorder handler

--- ClientUI/Program.cs
+++ ClientUI/Program.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using System.Threading.Tasks;
+using Messages;
 using NServiceBus;
+using NServiceBus.Logging;
 
 namespace ClientUI
 {
     class Program
     {
+        static ILog log = LogManager.GetLogger<Program>();
         static async Task Main(string[] args)
         {
             Console.Title = "ClientUI";
@@ -15,11 +18,41 @@ namespace ClientUI
             var endpointInstance = await Endpoint.Start(endpointConfiguration)
                 .ConfigureAwait(false);
             
-            Console.WriteLine("Press Enter to exit...");
-            Console.ReadLine();
+            await RunLoop(endpointInstance);
 
             await endpointInstance.Stop()
                 .ConfigureAwait(false);
+        }
+
+        static async Task RunLoop(IEndpointInstance endpointInstance)
+        {
+            while (true)
+            {
+                log.Info("Press 'P' to place an order, or 'Q' to quit.");
+                var key = Console.ReadKey();
+                Console.WriteLine();
+
+                switch (key.Key)
+                {
+                    case ConsoleKey.P:
+                        var command = new PlaceOrder
+                        {
+                            OrderId = Guid.NewGuid().ToString()
+                        };
+
+                        log.Info($"Sending PlaceOrder command, OrderId = {command.OrderId}");
+
+                        await endpointInstance.SendLocal(command)
+                            .ConfigureAwait(false);
+                        
+                        break;
+                    case ConsoleKey.Q:
+                        return;
+                    default:
+                        log.Info("Unknown input. Please try again.");
+                        break;
+                }
+            }
         }
     }
 }
